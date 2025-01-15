f5da8aff54982df9e76713f9603b32de2efcdaec
Saro G. <saro@saromedia.com>
2025-01-15 14:09:30 -0500

    Introduce FreeBSD build scripts

--- build-tools/src/main/java/org/elasticsearch/gradle/ElasticsearchDistribution.java
+++ build-tools/src/main/java/org/elasticsearch/gradle/ElasticsearchDistribution.java
@@ -27,7 +27,8 @@ public class ElasticsearchDistribution implements Buildable, Iterable<File> {
     public enum Platform {
         LINUX,
         WINDOWS,
-        DARWIN;
+        DARWIN,
+        FREEBSD;
 
         @Override
         public String toString() {
@@ -40,6 +41,7 @@ public class ElasticsearchDistribution implements Buildable, Iterable<File> {
         .onLinux(() -> Platform.LINUX)
         .onWindows(() -> Platform.WINDOWS)
         .onMac(() -> Platform.DARWIN)
+        .onFreeBSD(() -> Platform.FREEBSD)
         .supply();
 
     private final String name;
