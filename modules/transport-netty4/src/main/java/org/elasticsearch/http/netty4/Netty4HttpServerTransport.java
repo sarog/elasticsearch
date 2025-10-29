eeb903d291747e0150b1f471b41b13534341ca98
Saro G. <saro@saromedia.com>
2025-10-29 13:20:56 -0400

    Allow netty to set TCP settings on FreeBSD

--- modules/transport-netty4/src/main/java/org/elasticsearch/http/netty4/Netty4HttpServerTransport.java
+++ modules/transport-netty4/src/main/java/org/elasticsearch/http/netty4/Netty4HttpServerTransport.java
@@ -199,7 +199,7 @@ public class Netty4HttpServerTransport extends AbstractHttpServerTransport {
 
             if (SETTING_HTTP_TCP_KEEP_ALIVE.get(settings)) {
                 // Netty logs a warning if it can't set the option, so try this only on supported platforms
-                if (IOUtils.LINUX || IOUtils.MAC_OS_X) {
+                if (IOUtils.LINUX || IOUtils.MAC_OS_X || IOUtils.FREEBSD) {
                     if (SETTING_HTTP_TCP_KEEP_IDLE.get(settings) >= 0) {
                         serverBootstrap.childOption(
                             NioChannelOption.of(NetUtils.getTcpKeepIdleSocketOption()),
