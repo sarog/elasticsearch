1d98ed51c3620ec976f9989c7ffbb8aaf644318f
Saro G. <saro@saromedia.com>
2025-11-19 13:10:27 -0500

    Get system load average on FreeBSD

--- server/src/main/java/org/elasticsearch/monitor/os/OsProbe.java
+++ server/src/main/java/org/elasticsearch/monitor/os/OsProbe.java
@@ -291,7 +291,7 @@ public class OsProbe {
                 return null;
             }
         } else {
-            assert Constants.MAC_OS_X;
+            assert Constants.MAC_OS_X || Constants.FREE_BSD;
             if (getSystemLoadAverage == null) {
                 return null;
             }
