bdf5d8ecd954d106b5fbcb1c3777ffb74bffa2c7
Saro G. <saro@saromedia.com>
2026-08-25 16:49:21 -0400

    [tests] Run VectorSimilarityFunctions test on FreeBSD

--- libs/native/src/test/java/org/elasticsearch/nativeaccess/VectorSimilarityFunctionsTests.java
+++ libs/native/src/test/java/org/elasticsearch/nativeaccess/VectorSimilarityFunctionsTests.java
@@ -83,7 +83,7 @@ public abstract class VectorSimilarityFunctionsTests extends ESTestCase {
 
         if (jdkVersion >= 21
             && ((arch.equals("aarch64") && (osName.startsWith("Mac") || osName.equals("Linux")))
-                || (arch.equals("amd64") && osName.equals("Linux")))) {
+                || (arch.equals("amd64") && (osName.equals("Linux") || osName.equals("FreeBSD"))))) {
             assertThat(vectorSimilarityFunctions, isPresent());
             return true;
         } else {
